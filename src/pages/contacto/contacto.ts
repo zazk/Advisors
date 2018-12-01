1a130bd22f83dfe627c421feca1d0eb47c66fdce
Enrique Juan de Dios <juandedioz@gmail.com>
2018-12-01 10:59:45 -0300

    [Setup Ionic] Add user validate when getUsuario() fails, fix format

--- src/pages/contacto/contacto.ts
+++ src/pages/contacto/contacto.ts
@@ -1,8 +1,14 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
-import { NavController, NavParams, AlertController, LoadingController, ViewController } from 'ionic-angular';
-import { AuthService } from '../../providers/auth-service';
-import { Componentes } from '../../models/componentes'
-import { GlovalVars } from '../../providers/gloval-vars'
+import { Component, ViewChild, ElementRef } from "@angular/core";
+import {
+  NavController,
+  NavParams,
+  AlertController,
+  LoadingController,
+  ViewController
+} from "ionic-angular";
+import { AuthService } from "../../providers/auth-service";
+import { Componentes } from "../../models/componentes";
+import { GlovalVars } from "../../providers/gloval-vars";
 /*
   Generated class for the ContactoPage page.
 
@@ -10,43 +16,73 @@ import { GlovalVars } from '../../providers/gloval-vars'
   Ionic pages and navigation.
 */
 @Component({
-  selector: 'page-contacto',
-  templateUrl: 'contacto.html'
+  selector: "page-contacto",
+  templateUrl: "contacto.html"
 })
-
 export class ContactoPage {
-  @ViewChild('myInput') myInput: ElementRef;
-  data = { Usuario: '', DocumentoIdentidad : '', Remitente: '', Asunto: '', Mensaje: '' }
-  telefonos: [{ ciudad, numero, concatenado }]
-  
-  constructor(public navCtrl: NavController, public navParams: NavParams, public auth: AuthService,
-    private alertCtrl: AlertController, private loadingCtrl: LoadingController, public vars: GlovalVars,
-    public viewCtrl: ViewController) {
-      this.telefonos = [{ ciudad: '', numero: '', concatenado: '' }]
+  @ViewChild("myInput") myInput: ElementRef;
+  data = {
+    Usuario: "",
+    DocumentoIdentidad: "",
+    Remitente: "",
+    Asunto: "",
+    Mensaje: ""
+  };
+  telefonos: [{ ciudad; numero; concatenado }];
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public auth: AuthService,
+    private alertCtrl: AlertController,
+    private loadingCtrl: LoadingController,
+    public vars: GlovalVars,
+    public viewCtrl: ViewController
+  ) {
+    this.telefonos = [{ ciudad: "", numero: "", concatenado: "" }];
     this.telefonos.pop();
-    this.telefonos.push({ ciudad: 'Lima', numero: '(01) 200 4 200', concatenado: 'Lima (01) 200 4 200' });
-    this.telefonos.push({ ciudad: 'Arequipa', numero: '(054) 38 03 03', concatenado: 'Arequipa (054) 38 03 03' });
-    this.telefonos.push({ ciudad: 'Tacna', numero: '(052) 42 82 82', concatenado: 'Tacna (052) 42 82 82' });
-    this.telefonos.push({ ciudad: 'Ilo', numero: '(053) 48 28 36', concatenado: 'Ilo (053) 48 28 36' });
-    }
+    this.telefonos.push({
+      ciudad: "Lima",
+      numero: "(01) 200 4 200",
+      concatenado: "Lima (01) 200 4 200"
+    });
+    this.telefonos.push({
+      ciudad: "Arequipa",
+      numero: "(054) 38 03 03",
+      concatenado: "Arequipa (054) 38 03 03"
+    });
+    this.telefonos.push({
+      ciudad: "Tacna",
+      numero: "(052) 42 82 82",
+      concatenado: "Tacna (052) 42 82 82"
+    });
+    this.telefonos.push({
+      ciudad: "Ilo",
+      numero: "(053) 48 28 36",
+      concatenado: "Ilo (053) 48 28 36"
+    });
+  }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ContactoPagePage');
-    this.data.Remitente ='mail@cia.com';
-    this.data.DocumentoIdentidad = this.vars.getUsuario().userName;
-    this.data.Usuario = this.vars.getUsuario().Nombres;
-  }
-  resize() {
-    this.myInput.nativeElement.style.height = this.myInput.nativeElement.height + 'px';
-  }
-  
-  Enviar(){
-    var comp = new Componentes(this.alertCtrl, this.navCtrl);
-    if (this.data.Asunto === '' || this.data.Mensaje === '') {
-      comp.showPopup("Error", "Complete todos los campos para continuar.");
-    }else{      
-      
+    console.log("ionViewDidLoad ContactoPagePage");
+    this.data.Remitente = "mail@cia.com";
+    // TODO This was modified because fails when user is null
+    const user = this.vars.getUsuario();
+    if (user) {
+      this.data.DocumentoIdentidad = user.userName;
+      this.data.Usuario = user.Nombres;
     }
   }
+  resize() {
+    this.myInput.nativeElement.style.height =
+      this.myInput.nativeElement.height + "px";
+  }
 
+  Enviar() {
+    var comp = new Componentes(this.alertCtrl, this.navCtrl);
+    if (this.data.Asunto === "" || this.data.Mensaje === "") {
+      comp.showPopup("Error", "Complete todos los campos para continuar.");
+    } else {
+    }
+  }
 }
