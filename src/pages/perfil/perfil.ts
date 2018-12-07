aef794aad4eeb934bb137f3ca5399ac8958c7f88
Enrique Juan de Dios <juandedioz@gmail.com>
2018-12-06 23:42:40 -0300

    [Hotfix] Replace Modal with Push in Update Profile

--- src/pages/perfil/perfil.ts
+++ src/pages/perfil/perfil.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams, ModalController } from 'ionic-angular';
-import { ModalDatosPage } from '../modal-datos/modal-datos'
-import { ModalPasswordPage } from '../modal-password/modal-password'
-import { ModalPreguntaPage } from '../modal-pregunta/modal-pregunta'
-import { AuthService } from '../../providers/auth-service';
-import { LoginPage } from '../login/login';
+import { Component } from "@angular/core";
+import { NavController, NavParams, ModalController } from "ionic-angular";
+import { ModalDatosPage } from "../modal-datos/modal-datos";
+import { ModalPasswordPage } from "../modal-password/modal-password";
+import { ModalPreguntaPage } from "../modal-pregunta/modal-pregunta";
+import { AuthService } from "../../providers/auth-service";
+import { LoginPage } from "../login/login";
 
 /*
   Generated class for the Perfil page.
@@ -13,21 +13,28 @@ import { LoginPage } from '../login/login';
   Ionic pages and navigation.
 */
 @Component({
-  selector: 'page-perfil',
-  templateUrl: 'perfil.html'
+  selector: "page-perfil",
+  templateUrl: "perfil.html"
 })
 export class PerfilPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams
-    , public modalCtrl: ModalController, public auth: AuthService) { }
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public modalCtrl: ModalController,
+    public auth: AuthService
+  ) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad PerfilPage');
+    console.log("ionViewDidLoad PerfilPage");
   }
 
   openModalDatos() {
-    let modal = this.modalCtrl.create(ModalDatosPage);
-    modal.present();
+    // When you want to open a page in a modal use the next two lines
+    // let modal = this.modalCtrl.create(ModalDatosPage);
+    // modal.present();
+
+    // If you wanna to push a Page in your navigation use this:
+    this.navCtrl.push(ModalDatosPage);
   }
 
   openModalPassword() {
@@ -42,7 +49,7 @@ export class PerfilPage {
 
   public logout() {
     this.auth.logout().subscribe(succ => {
-      this.navCtrl.setRoot(LoginPage)
+      this.navCtrl.setRoot(LoginPage);
     });
   }
 }
